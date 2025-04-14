b02f96a09a7cca80542c0252fb52efd345ff464b
ayaz-125 <ayazr425@gmail.com>
2025-04-14 12:22:21 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is the end to end mlops project using th different tools and the different algorithm',
+    author='ayaz rabbani',
+    license='MIT',
+)
